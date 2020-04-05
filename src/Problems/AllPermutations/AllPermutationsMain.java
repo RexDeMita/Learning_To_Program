4988a1350d24398031fd87c74a67ec930f5f6523
furry <mita.rakotoma@gmail.com>
2020-04-05 17:55:51 -0500

    finished 3 distinct object permutation generator

--- src/Problems/AllPermutations/AllPermutationsMain.java
+++ src/Problems/AllPermutations/AllPermutationsMain.java
@@ -5,73 +5,60 @@ package Problems.AllPermutations;
 public class AllPermutationsMain {
 
     public static void main (String [] args) {
-
+        //This is the string whose permutations are found
         String string = "123";
-        int n = string.length();
-        StringBuilder stringToSwap = new StringBuilder(string);
-       // System.out.println(factorial(4));
-        System.out.println(threeObjectPermutationGenerator(string, n));
-       // swap(string, 0, 1);
-       // System.out.println(oneIndexToTheRight(string));
+
+        //Prints out all the permutations of the string
+        System.out.println(threeDistinctObjectPermutationGenerator(string));
 
     }
 
-    public static String threeObjectPermutationGenerator (String string, int n) {
+    public static String threeDistinctObjectPermutationGenerator (String string) {
+
         System.out.println(string);
-        //this for loop is meant to isolate each element so that the other elements can be swapped
-       // for (int i = 0; i < n; i++){
+        //first permutation
+
         String stringToSwap = swap(string, 0, 1);
         System.out.println(stringToSwap);
+        //second permutation
+
         stringToSwap = oneIndexToTheRight(stringToSwap);
         System.out.println(stringToSwap);
+        //third permutation
+
         stringToSwap = swap(stringToSwap, 0, 1);
         System.out.println(stringToSwap);
+        //fourth permutation
+
         stringToSwap = oneIndexToTheLeft(stringToSwap);
         System.out.println(stringToSwap);
+        //fifth permutation
+
         stringToSwap = swap(stringToSwap, 0, 1);
+        //this return is the final permutation which gets printed out by the method call
 
-
-
-
-
-/*
-                if (n % 2 != 0){
-                    swap(string, 0, n);
-                }
-                else {
-                    for (int j = 0; j < n; j++){
-                        swap(string, j, n);
-                    }
-                }
-                n--;*/
-        //}
         return (stringToSwap);
+
     }
+
 
     public static String swap (String string, int j, int k){
 
-        //System.out.println(string);
 
         //A string builder is initialized so that the string whose permutations are being found can be edited and returned
         StringBuilder stringToSwap = new StringBuilder(string);
 
         //first character of the string at index 0
         char a = string.charAt(j);
-       // System.out.println(a);
 
         //the second character at index 1
         char b = string.charAt(k);
-       // System.out.println(b);
 
         //changing the character at index 0 to the value of b
         stringToSwap.setCharAt(j, b);
 
-       // System.out.println(stringToSwap);
-
         //changing the character at index j to the value of a
         stringToSwap.setCharAt(k, a);
-
-       // System.out.println(stringToSwap);
 
         //conversion of StringBuilder into string
         String swappedString = stringToSwap.toString();
@@ -80,15 +67,25 @@ public class AllPermutationsMain {
     }
 
     public static String oneIndexToTheRight (String string){
+
+        //moves the characters one index to the right
+
+        //adds the last character to the beginning of the string
         String newString = string.charAt(string.length() - 1) + string;
-        //System.out.println(newString);
+
+        //creates a substring that excludes the last character
         newString = newString.substring(0, (newString.length() - 1));
-       // System.out.println(newString);
+
         return newString;
     }
 
     public static String oneIndexToTheLeft (String string){
+
+        //moves the characters to the left
+
+        //Adds the first character to the end of a substring starting at index 1
         String newString = string.substring(1) + string.charAt(0);
+
         return newString;
     }
 
